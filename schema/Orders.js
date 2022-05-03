eaeee58c9fd880c41c428d4b35cf9f95ac340ba4
system <system@cubecloud.dev>
2022-05-03 08:25:18 +0000

    1

--- schema/Orders.js
+++ schema/Orders.js
@@ -12,7 +12,6 @@ cube(`Orders`, {
     `,
   
     preAggregations: {
-      //
       // Pre-Aggregations definitions go here
       // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
     },
